26905c913956d1f7334332fb935d27a70fe2d6e6
romanyshyn97 <romanyshyn.ol.rom@gmail.com>
2022-05-11 22:35:20 +0300

    make project template

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employees acounting in company</h1>
+            <h2>Total number of employees: </h2>
+            <h2>Will receive premium</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
